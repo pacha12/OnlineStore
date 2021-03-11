1faded6f001f284ce55ea01c3270a0b4b91c4a0c
pacha12 <43570362+pacha12@users.noreply.github.com>
2021-03-11 16:15:39 +0200

    Add Login and Register Identity and View Model Login and Register
    Add Login and Register Identity and View Model Login and Register

---
+++ OnlineStore/OnlineStore_UI/Models/RegisterViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineStore_UI.Models
+{
+    public class RegisterViewModel
+    {
+    }
+}
